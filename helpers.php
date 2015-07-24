7d4e5fc1ca16886faa8efc0fb9c9fd820e56864b
Radu <radu.bordei89@gmail.com>
2015-07-24 14:13:08 +0300

    rename functions.

--- helpers.php
+++ helpers.php
@@ -3,13 +3,28 @@
 namespace Flysal\Administrator;
 
 /**
+ * Get menu sections .
+ *
+ * @return mixed
+ */
+function get_menu_sections() {
+    $manager = app('menu-manager');
+
+    return $manager->getGroups();
+}
+
+/**
  * Render section .
  *
  * @param $section
  * @param $manager
+ * @param array $attributes
+ * @return
  */
-function render_section($section, $manager) {
-    return $manager->render($section);
+function render_menu_section($section, $attributes = array()) {
+    $manager = app('menu-manager');
+
+    return $manager->render($section, $attributes);
 }
 
 /**

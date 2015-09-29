b190416fc6021d315c0c36933321e48a4d803194
Radu <radu.bordei89@gmail.com>
2015-09-29 12:03:20 +0300

    rewrite getLogin function.

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -11,11 +11,38 @@ class AuthController extends Controller {
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     /**
+     * @var string
+     */
+    protected $redirectPath = '/admin';
+
+    /**
+     * @var string
+     */
+    protected $loginPath = '/admin/login';
+
+
+    /**
      * Create a new authentication controller instance.
      *
      */
     public function __construct() {
         $this->middleware('guest', ['except' => 'getLogout']);
+    }
+
+    /**
+     * Get login form .
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getLogin() {
+        if (view()->exists('themes::auth.login'))
+            return view('themes::auth.login');
+
+        if (view()->exists('auth.authenticate')) {
+            return view('auth.authenticate');
+        }
+
+        return view('auth.login');
     }
 
 
